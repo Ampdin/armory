f7d0e08ad1fda022fb29d0b82ea03fdf2bb47af7
spirovanni <x@ampdin.com>
2017-09-16 23:06:01 -0700

    rename of navbar
    Needed to be sidenav.component.ts

---
+++ src/main/webapp/app/layouts/sidebar/sidenav.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'jhi-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: [
+        'sidebar.scss'
+    ]
+})
+export class SidebarComponent implements OnInit {
+    ngOnInit(): void {}
+}
