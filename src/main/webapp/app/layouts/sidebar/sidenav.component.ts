4880ba7c3cde11d000ce7b57171e00c0c5f2f13e
spirovanni <x@ampdin.com>
2017-09-16 23:58:19 -0700

    Added to Sidenav
    updated Sidenav and version

--- src/main/webapp/app/layouts/sidebar/sidenav.component.ts
+++ src/main/webapp/app/layouts/sidebar/sidenav.component.ts
@@ -1,4 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { JhiLanguageService } from 'ng-jhipster';
+
+import { ProfileService } from '../profiles/profile.service';
+import { JhiLanguageHelper, Principal, LoginModalService, LoginService } from '../../shared';
+
+import { VERSION, DEBUG_INFO_ENABLED } from '../../app.constants';
 
 @Component({
     selector: 'jhi-sidebar',
@@ -8,5 +16,65 @@ import {Component, OnInit} from '@angular/core';
     ]
 })
 export class SidebarComponent implements OnInit {
-    ngOnInit(): void {}
+
+    inProduction: boolean;
+    isSidenavCollapsed: boolean;
+    languages: any[];
+    swaggerEnabled: boolean;
+    modalRef: NgbModalRef;
+    version: string;
+
+    constructor(
+        private loginService: LoginService,
+        private languageService: JhiLanguageService,
+        private languageHelper: JhiLanguageHelper,
+        private principal: Principal,
+        private loginModalService: LoginModalService,
+        private profileService: ProfileService,
+        private router: Router
+    ) {
+        this.version = VERSION ? 'v' + VERSION : '';
+        this.isSidenavCollapsed = true;
+    }
+
+    ngOnInit() {
+        this.languageHelper.getAll().then((languages) => {
+            this.languages = languages;
+        });
+
+        this.profileService.getProfileInfo().subscribe((profileInfo) => {
+            this.inProduction = profileInfo.inProduction;
+            this.swaggerEnabled = profileInfo.swaggerEnabled;
+        });
+    }
+
+    changeLanguage(languageKey: string) {
+        this.languageService.changeLanguage(languageKey);
+    }
+
+    collapseSidenav() {
+        this.isSidenavCollapsed = true;
+    }
+
+    isAuthenticated() {
+        return this.principal.isAuthenticated();
+    }
+
+    login() {
+        this.modalRef = this.loginModalService.open();
+    }
+
+    logout() {
+        this.collapseSidenav();
+        this.loginService.logout();
+        this.router.navigate(['']);
+    }
+
+    toggleSidenav() {
+        this.isSidenavCollapsed = !this.isSidenavCollapsed;
+    }
+
+    getImageUrl() {
+        return this.isAuthenticated() ? this.principal.getImageUrl() : null;
+    }
 }
